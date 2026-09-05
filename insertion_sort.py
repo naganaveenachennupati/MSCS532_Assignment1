a3e28aa0c26d3a45bbc31786b23c1814d5479c31
Naga Naveena Chennupati <nchennupati63666@ucumberlands.edu>
2026-09-05 18:43:23 -0500

    Create initial Python program for Assignment 1

---
+++ insertion_sort.py
@@ -0,0 +1,19 @@
+"""
+MSCS 532 - Assignment 1
+Naga Naveena Chennupati
+
+Insertion Sort in decreasing order.
+"""
+
+
+def insertion_sort_descending(values):
+    pass
+
+
+def main():
+    sample_values = [12, 7, 19, 3, 15, 8]
+    print(sample_values)
+
+
+if __name__ == "__main__":
+    main()
